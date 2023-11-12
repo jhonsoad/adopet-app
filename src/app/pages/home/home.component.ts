f8e56afa22c4283704847f019db6520d0ec7f1cb
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2023-11-12 20:39:22 -0300

    colocando cabecalho na url da imagem

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from 'src/app/interface/post';
@@ -49,4 +50,10 @@ export class MobileHomeComponent implements OnInit {
     this.roter.navigate(['/mensagem']);
   }
 
+  getPetImageUrl(imageName: string): string {
+    const imageUrl = this.variableValue + 'publi/images/' + imageName;
+    const headers = new HttpHeaders().set('ngrok-skip-browser-warning', 'true');
+    return imageUrl + '?' + headers.keys().map(key => `${key}=${headers.get(key)}`).join('&');
+  }
+  
 }
